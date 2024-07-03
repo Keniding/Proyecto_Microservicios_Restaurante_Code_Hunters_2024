2ee55f59fe7d95d49089cc683caf79b9bf44cdb9
keniding <U20310552@utp.edu.pe>
2024-07-03 16:19:24 -0500

    Restructuración de rutas
    Cambio en el backend, trabajaré con servicios levantados por puertos, asi desacoplare  las dependencias de recursos por direcciones de archivo a URL, creare servicios REST

--- restaurant-system/backend/api/indice/controller/controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    header('Location: ../../../../frontend/app/login/login.php');
-    exit();
-}
-?>
--- restaurant-system/backend/interface/iUserService.php
+++
@@ -1,6 +0,0 @@
-<?php
-interface iUserService
-{
-    public function getUserById($id);
-    public function getUserByUsername($name);
-}
